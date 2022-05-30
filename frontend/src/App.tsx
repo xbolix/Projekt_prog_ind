f108a3fe2f39cf9faa4879773765585581e046d6
xbolix <102685012+xbolix@users.noreply.github.com>
2022-05-30 21:00:26 +0200

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  DnsQueries,
+  DnsQueryForm,
+  Footer,
+  Header,
+  NTPQueryForm,
+} from "./components";
+import "./App.css";
+import { Grid } from "@mui/material";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Header />
+        </Grid>
+        <Grid item xs={12} md={4} xl={3}>
+          <DnsQueryForm />
+          <div className="pt-7" />
+          <NTPQueryForm />
+        </Grid>
+        <Grid item xs={12} md={8} xl={9}>
+          <DnsQueries />
+        </Grid>
+        <Grid item xs={12}>
+          <Footer />
+        </Grid>
+      </Grid>
+      <ToastContainer position="bottom-right" />
+    </>
   );
 }
 
