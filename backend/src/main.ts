148fbe648fdf11c539dbd86dd6f8ed79074080b0
Patryk Mróz <patrykmroz619@wp.pl>
2022-05-30 20:57:55 +0200

    Configure backend app

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(8000);
+}
+bootstrap();
